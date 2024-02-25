025051c3e19cc44cb0029a535b61c7253f75e1ca
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-02-25 18:38:21 +0600

    Product model Set

--- OnlineShop/Data/ApplicationDbContext.cs
+++ OnlineShop/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using OnlineShop.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,5 +13,6 @@ namespace OnlineShop.Data
             : base(options)
         {
         }
+        public DbSet<ProductTypes> ProductTypes { get; set; }
     }
 }
---
+++ OnlineShop/Models/ProductTypes.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Models
+{
+    public class ProductTypes
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Product Type")]
+        public string ProductType { get; set; }
+    }
+}
