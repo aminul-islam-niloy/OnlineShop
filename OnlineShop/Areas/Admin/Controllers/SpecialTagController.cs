3520c01827bf75a99ddbcfc2a5c139930cfebb15
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-02-27 19:30:35 +0600

    Special Tage Controller and Index Generate

---
+++ OnlineShop/Areas/Admin/Controllers/SpecialTagController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data;
+using System.Linq;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SpecialTagController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public SpecialTagController(ApplicationDbContext db)
+        {
+            _db = db;   
+        }
+
+
+        public IActionResult Index()
+        {
+            var data = _db.SpecialTag.ToList();
+            return View(data);
+           
+        }
+    }
+}
