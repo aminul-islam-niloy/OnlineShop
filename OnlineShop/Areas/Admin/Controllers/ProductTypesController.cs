f324beb64b0da290d716dc770341ec3a2593473a
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-02-27 18:48:31 +0600

    Index for Product Types

--- OnlineShop/Areas/Admin/Controllers/ProductTypesController.cs
+++ OnlineShop/Areas/Admin/Controllers/ProductTypesController.cs
@@ -41,5 +41,117 @@ namespace OnlineShop.Areas.Admin.Controllers
         
             return View(productTypes);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var productType= _db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ProductTypes productTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productTypes);
+        }
+
+        //GET Details Action Method
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = _db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        //POST Edit Action Method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Details(ProductTypes productTypes)
+        {
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+
+        //GET Delete Action Method
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = _db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+
+        //POST Delete Action Method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int? id, ProductTypes productTypes)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            if (id != productTypes.Id)
+            {
+                return NotFound();
+            }
+
+            var productType = _db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Remove(productType);
+                await _db.SaveChangesAsync();
+                TempData["delete"] = "Product type has been deleted";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productTypes);
+        }
+
+
+
+
     }
 }
