07fc03c833819d235b38307b4e4db6f6e43467b9
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-02-28 01:29:48 +0600

    Procudt  Model,DbSet and Migration

--- OnlineShop/Data/ApplicationDbContext.cs
+++ OnlineShop/Data/ApplicationDbContext.cs
@@ -15,6 +15,7 @@ namespace OnlineShop.Data
         }
         public DbSet<ProductTypes> ProductTypes { get; set; }
         public DbSet<SpecialTag> SpecialTag { get; set; }
+        public DbSet<Products> Products { get; set; }
 
     }
 }
---
+++ OnlineShop/Models/Products.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+        [Display(Name = "Product Color")]
+        public string ProductColor { get; set; }
+        [Required]
+        [Display(Name = "Available")]
+        public bool IsAvailable { get; set; }
+
+        [Display(Name = "Product Type")]
+        [Required]
+        public int ProductTypeId { get; set; }
+        [ForeignKey("ProductTypeId")]
+        public virtual ProductTypes ProductTypes { get; set; }
+        [Display(Name = "Special Tag")]
+        [Required]
+        public int SpecialTagId { get; set; }
+        [ForeignKey("SpecialTagId")]
+        public virtual SpecialTag SpecialTag { get; set; }
+
+    }
+}
