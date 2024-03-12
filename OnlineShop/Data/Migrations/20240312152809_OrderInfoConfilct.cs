cd475f996afcf28b413fef0338cc14b2b91b4450
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-03-12 23:03:14 +0600

    Order Quantity Confict

---
+++ OnlineShop/Data/Migrations/20240312152809_OrderInfoConfilct.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineShop.Data.Migrations
+{
+    public partial class OrderInfoConfilct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "OrderDetails",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "OrderDetails");
+        }
+    }
+}
---
+++ OnlineShop/Models/OrderViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace OnlineShop.Models
+{
+    public class OrderViewModel
+    {
+        public int OrderId { get; set; }
+        public string OrderNo { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerPhone { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+
+        public List<ProductViewModel> Products { get; set; }
+    }
+}
